ed855562509d188cef3078efc3b235155a33a414
Spencer Lee <spencerlee96@gmail.com>
2020-07-05 23:19:38 -0600

    added twilio

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+require('dotenv').config()
+
 const express = require('express');
 const path = require('path');
 const generatePassword = require('password-generator');
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+const tc = require('./twilioController')
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(pino);
+
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+//app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
 app.get('/api/passwords', (req, res) => {
@@ -22,13 +31,17 @@ app.get('/api/passwords', (req, res) => {
   console.log(`Sent ${count} passwords`);
 });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+// // The "catchall" handler: for any request that doesn't
+// // match one above, send back React's index.html file.
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log(`Password generator listening on ${port}`);
+
+//twilio endpoint
+app.post('/api/text', tc.text)
+app.post('/api/send', tc.send)
---
+++ twilioController.js
@@ -0,0 +1,42 @@
+const {TWILIO_AUTH_TOKEN, TWILIO_ACCOUNT_SID} = process.env
+const accountSid = TWILIO_ACCOUNT_SID;
+const authToken = TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+module.exports = {
+    send: async(req, res) => { //currently unused, will switch over to this controller once I've figured out how to format the front end the way I'd like it
+        res.header('Content-Type', 'application/json');
+        const {messageBody} = req.body
+        client.messages
+            .create({
+                from: process.env.TWILIO_PHONE_NUMBER,
+                to: process.env.COMPANY_PHONE_NUMBER,
+                body: "hello there"
+            })
+            .then(() => {
+                res.send(JSON.stringify({ success: true }));
+            })
+            .catch(err => {
+                console.log(err);
+                res.send(JSON.stringify({ success: false }));
+            }
+        );
+    },
+
+    text: async(req, res) => { //sends text to company number
+        const accountSid = TWILIO_ACCOUNT_SID;
+        const authToken = TWILIO_AUTH_TOKEN;
+        const client = require('twilio')(accountSid, authToken);
+
+        const {messageBody} = req.body
+    
+        client.messages
+            .create({
+                body: messageBody,
+                from: process.env.TWILIO_PHONE_NUMBER,
+                to: process.env.COMPANY_PHONE_NUMBER
+            })
+            .then(message => console.log(message.sid));
+            res.status(200).send("successful")
+    }
+}
